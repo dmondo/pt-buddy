5598f7c1f4b42985db1a2d9ea92f6c10e9193247
dmondo <dannysmondo@gmail.com>
2020-06-17 22:39:58 -0700

    refactor(index.tsx): index is wrapped in store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import { StoreProvider } from './store/store';
 
 ReactDOM.render(
-  <App />,
+  <StoreProvider>
+    <App />
+  </StoreProvider>,
   document.getElementById('watchdog'),
 );
