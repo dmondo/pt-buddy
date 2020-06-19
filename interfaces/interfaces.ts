aea70ee913aaaac10456cd4b3acffb0024aef68f
dmondo <dannysmondo@gmail.com>
2020-06-18 20:34:15 -0700

    feat(interface): typing for reminder and patient objects

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -3,6 +3,21 @@ interface IState {
   loginStatus: boolean;
   failedLogin: boolean;
   newUser: boolean;
+  scheduling: boolean;
+  patients: IPatient[];
+  reminders: IReminder[];
+}
+
+interface IReminder {
+  tag: string;
+  text: string;
+  uuid: string;
+}
+
+interface IPatient {
+  name: string;
+  phone: number;
+  uuid: string;
 }
 
 interface IAction {
