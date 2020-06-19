bd009c99d635ed1b62fee9a7a7b077163a9b3b28
dmondo <dannysmondo@gmail.com>
2020-06-18 20:36:20 -0700

    feat(history): history bar contains saved patients, reminders from store

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { store } from '../store/store';
+import Reminders from './reminders';
+import Patients from './patients';
 
 const useStyles = makeStyles({
   root: {
@@ -26,7 +27,9 @@ const useStyles = makeStyles({
 
 const History = (): JSX.Element => {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+  // const bull = <span className={classes.bullet}>•</span>;
+  const { state } = React.useContext(store);
+  const { reminders, patients } = state;
 
   return (
     <Grid container spacing={1} alignItems="flex-end">
@@ -36,10 +39,14 @@ const History = (): JSX.Element => {
             <Typography variant="h5" component="h2">
               your reminders
             </Typography>
-            <Typography variant="body2" component="p">
-              {bull}
-              blah blah blah.
-            </Typography>
+            {
+              reminders.map((reminder: IReminder) => (
+                <Reminders
+                  reminder={reminder}
+                  key={reminder.uuid}
+                />
+              ))
+            }
           </CardContent>
         </Card>
       </Grid>
@@ -49,10 +56,18 @@ const History = (): JSX.Element => {
             <Typography variant="h5" component="h2">
               your patients
             </Typography>
-            <Typography variant="body2" component="p">
+            {/* <Typography variant="body2" component="p">
               {bull}
               name 123-345-6789
-            </Typography>
+            </Typography> */}
+            {
+              patients.map((patient: IPatient) => (
+                <Patients
+                  patient={patient}
+                  key={patient.uuid}
+                />
+              ))
+            }
           </CardContent>
         </Card>
       </Grid>
