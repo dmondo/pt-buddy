2fab5337be9060540af70ac9095c5ef45195d982
dmondo <dannysmondo@gmail.com>
2020-06-21 22:01:46 -0700

    feat(model): time props and validation for reminder model

--- database/models/reminders.ts
+++ database/models/reminders.ts
@@ -4,13 +4,47 @@ const validPhone = (val: string): boolean => (
   val.length === 12 && val[0] === '+'
 );
 
+const validTime = (val: string): boolean => {
+  if (
+    Number.isNaN(Number(val[0]))
+    || Number.isNaN(Number(val[1]))
+    || Number.isNaN(Number(val[3]))
+    || Number.isNaN(Number(val[4]))
+  ) {
+    return false;
+  }
+
+  if (val[2] !== ':') { return false; }
+  if (
+    Number(val[0]) < 0
+    || Number(val[0]) > 2
+    || Number(val[1]) < 0
+    || Number(val[3]) < 0
+    || Number(val[3]) > 5
+    || Number(val[4]) < 0
+  ) {
+    return false;
+  }
+  return true;
+};
+
+const validAMPM = (val: string): boolean => (
+  val === 'am' || val === 'pm'
+);
+
 const reminderSchema = new mongoose.Schema({
   uuid: String,
   ptuuid: String,
+  jobid: String,
   tag: String,
   text: String,
   date: Date,
+  time: { type: String, validate: validTime },
+  ampm: { type: String, validate: validAMPM },
+  daily: Boolean,
   patientNumber: { type: String, validate: validPhone },
+  patientName: String,
+  completed: Boolean,
 });
 
 const Reminder = mongoose.model('Reminder', reminderSchema);
