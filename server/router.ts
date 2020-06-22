1729e1a7b7229d17b7a1f08e00b48b01f1868081
dmondo <dannysmondo@gmail.com>
2020-06-22 13:18:48 -0700

    refactor(routes): delete unused internal route

--- server/router.ts
+++ server/router.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import path from 'path';
-import { postReminder, getReminders, deleteReminder } from './controllers/reminders';
+import { postReminder, deleteReminder } from './controllers/reminders';
 import { getUser, postUser } from './controllers/users';
 
 const router = Router();
@@ -9,7 +9,6 @@ router.get('/', (req: Request, res: Response): void => {
   res.sendFile(path.join(__dirname, '..', 'src', 'index.html'));
 });
 
-router.get('/reminders', getReminders);
 router.post('/reminders', postReminder);
 router.delete('/reminders', deleteReminder);
 
