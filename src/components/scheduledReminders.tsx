10f9d5a37233aa58e64eb22b21ce5303dc450185
dmondo <dannysmondo@gmail.com>
2020-06-21 17:45:34 -0700

    feat(scheduledReminders): reminders can be deleted locally and in db

--- src/components/scheduledReminders.tsx
+++ src/components/scheduledReminders.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import { store } from '../store/store';
 
 const useStyles = makeStyles({
   root: {
@@ -22,12 +25,42 @@ const useStyles = makeStyles({
 const ScheduledReminders = ({ scheduled }: { scheduled: IScheduled }): JSX.Element => {
   const classes = useStyles();
   const bull = <span className={classes.bullet}>•</span>;
+  const { state, dispatch } = React.useContext(store);
+
+  const deleteReminder = async (): Promise<void> => {
+    const { scheduledReminders } = state;
+    const newReminders = [];
+    scheduledReminders.forEach((reminder: IScheduled) => {
+      if (reminder.uuid !== scheduled.uuid) {
+        newReminders.push(reminder);
+      }
+    });
+
+    dispatch({ type: 'SCHEDULED', payload: newReminders });
+
+    const url = '/reminders';
+    const options = {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ uuid: scheduled.uuid }),
+    };
+    await fetch(url, options);
+  };
+
   return (
     <>
       <Typography variant="body2" component="p">
         {bull}
         {`${scheduled.tag} - ${scheduled.patients} - ${scheduled.day} - ${scheduled.time}`}
       </Typography>
+      <CardActions>
+        <Button
+          size="small"
+          onClick={deleteReminder}
+        >
+          delete
+        </Button>
+      </CardActions>
     </>
   );
 };
