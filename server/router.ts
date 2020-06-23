5d4ba2ad85d15ed95386bcc823823bfe5d428321
dmondo <dannysmondo@gmail.com>
2020-06-22 18:10:51 -0700

    feat(router): rest API for post, get req on tags, patients

--- server/router.ts
+++ server/router.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from 'express';
 import path from 'path';
 import { postReminder, deleteReminder } from './controllers/reminders';
 import { getUser, postUser } from './controllers/users';
+import { getTags, postTags } from './controllers/tags';
+import { getPatients, postPatients } from './controllers/patients';
 
 const router = Router();
 
@@ -12,7 +14,13 @@ router.get('/', (req: Request, res: Response): void => {
 router.post('/reminders', postReminder);
 router.delete('/reminders', deleteReminder);
 
-router.get('/users', getUser);
+router.post('/auth', getUser);
 router.post('/users', postUser);
+
+router.get('/tags/:ptuuid', getTags);
+router.post('/tags', postTags);
+
+router.get('/patients/:ptuuid', getPatients);
+router.post('/patients', postPatients);
 
 export default router;
